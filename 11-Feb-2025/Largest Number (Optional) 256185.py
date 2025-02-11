24a4946cfc4e713a48f948508da108c5cda00d9f
JohnnyWorku <johnnythegold41@gmail.com>
2025-02-11 13:58:38 +0300

    Created 'Largest Number (Optional)' by A2SV Hub

---
+++ 11-Feb-2025/Largest Number (Optional) 256185.py
@@ -0,0 +1,23 @@
+# Problem: Largest Number (Optional) - https://leetcode.com/problems/largest-number/
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        result = ""
+        for i in range(len(nums)):
+            for j in range(len(nums) - i - 1):
+                if f"{nums[j]}{nums[j + 1]}" > f"{nums[j + 1]}{nums[j]}":
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        
+        nums.reverse()
+        
+        for num in nums:
+            result += str(num)
+
+        i = 0
+        if len(result) > 1:
+            while i < len(result) and result[i] == "0":
+                i += 1
+        result = result[i:]
+
+
+        return "0" if len(result) == 0 else result
