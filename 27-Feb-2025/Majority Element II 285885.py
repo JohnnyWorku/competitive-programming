30c34dbf765d5a15234e405d5eac0ea52f404146
Yohannes Worku <johnnythegold41@gmail.com>
2025-02-27 13:36:38 +0300

    Created 'Majority Element II' by A2SV Hub

---
+++ 27-Feb-2025/Majority Element II 285885.py
@@ -0,0 +1,21 @@
+# Problem: Majority Element II - https://leetcode.com/problems/majority-element-ii/?envType=daily-question&envId=2023-10-05
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        num_counter = defaultdict(int)
+
+        for num in nums:
+            num_counter[num] += 1
+            if len(num_counter) > 2:
+                new_counter = defaultdict(int)
+                for key, value in num_counter.items():
+                    if value > 1:
+                        new_counter[key] = value - 1
+                num_counter = new_counter
+                    
+        res = []
+        for num in num_counter:
+            if nums.count(num) > len(nums) // 3:
+                res.append(num)
+
+        return res
