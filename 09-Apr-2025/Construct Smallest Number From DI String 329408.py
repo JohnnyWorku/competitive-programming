08626fc312b7c8dbcf8a07f6ad25262f8d20dbd6
Yohannes Worku <johnnythegold41@gmail.com>
2025-04-09 10:12:49 +0300

    Created 'Construct Smallest Number From DI String' by A2SV Hub

---
+++ 09-Apr-2025/Construct Smallest Number From DI String 329408.py
@@ -0,0 +1,15 @@
+# Problem: Construct Smallest Number From DI String - https://leetcode.com/problems/construct-smallest-number-from-di-string/description/
+
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        # Stack and greedy concept
+        res, stack = [], []
+
+        for i in range(len(pattern) + 1):
+            stack.append(i + 1)
+
+            while stack and (i == len(pattern) or pattern[i] == "I"):
+                res.append(str(stack.pop()))
+
+        return "".join(res)
+        
